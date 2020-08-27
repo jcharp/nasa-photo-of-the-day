093411999cdb09ef35f5674940c466ef8419706e
unknown <jackcharp69@gmail.com>
2020-08-27 19:48:12 -0400

    starting over

--- src/PhotoInfo.js
+++ src/PhotoInfo.js
@@ -1,14 +1,17 @@
 import React ,{ useState, useEffect} from "react";
 import axios from "axios";
+import DateAndTitle from "./Title_and_Date";
+import PhotoAndDesc from "./Photo_and_description";
 
 export default function PhotoInfo() {
     const [data, setData] = useState([]);
     
     useEffect(() => {
         axios
-        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-07-06")
-            .then (res => {
+        .get('https://api.nasa.gov/planetary/apod?api_key=wimELLEq4Q598nHUaVITgDR2m69O1NAhe7qtfIGe&date=2020-08-25')
+            .then ((res) => {
                 console.log("list from NASA", res);
+                setData(res.Data);
                 
             })
             .catch(err => {
@@ -19,6 +22,16 @@ export default function PhotoInfo() {
 
     return (
         <div>
+            // return (
+    //     <div>
+                <DateAndTitle
+                date ={data.date}
+                title = {data.title}
+            />
+             <PhotoAndDesc
+                 photo = {data.url}
+                 desc = {data.explanation}
+             /></div>
 
         </div>
     )
