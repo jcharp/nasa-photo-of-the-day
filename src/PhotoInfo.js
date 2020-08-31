591753e61d52334210afee09675663791b220011
unknown <jackcharp69@gmail.com>
2020-08-30 22:07:10 -0400

    styled components done. MVP met

--- src/PhotoInfo.js
+++ src/PhotoInfo.js
@@ -3,6 +3,25 @@ import React, { useState, useEffect} from "react";
 import axios from "axios";
 import DateAndTitle from "./Date_and_Title";
 import PhotoAndDes from './Photo_and_description';
+import styled from 'styled-components';
+
+
+//building out styled component
+const WrapperDiv = styled.div`
+    font-family: cursive;
+    text-align: center;
+    padding: 2px;
+   
+    // nesting image styling
+    
+    img{
+        max-width: 70%;
+        height: auto;
+        margin: 30px;
+    }
+`;
+
+
 
 
 export default function PhotoInfo() {
@@ -10,7 +29,7 @@ export default function PhotoInfo() {
     
     useEffect(() => {
         axios
-        .get('https://api.nasa.gov/planetary/apod?api_key=wimELLEq4Q598nHUaVITgDR2m69O1NAhe7qtfIGe&date=2012-03-14')
+        .get('https://api.nasa.gov/planetary/apod?api_key=wimELLEq4Q598nHUaVITgDR2m69O1NAhe7qtfIGe&date=2020-03-14')
             .then ((res) => {
                 console.log("list from NASA", res);
                 console.log(res.data);
@@ -23,7 +42,9 @@ export default function PhotoInfo() {
     },[])
 
     return (
-        <div>
+
+        <WrapperDiv className="App">
+            <div>
             <DateAndTitle
             date={data.date}
             title={data.title}
@@ -36,6 +57,9 @@ export default function PhotoInfo() {
 
 
         </div>
+
+        </WrapperDiv>
+        
     )
 
 }
